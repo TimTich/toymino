7654f77ba2da92dd0e049f9650dd36cd0877f513
TimothyTichelaar <21108366@student.hhs.nl>
2026-07-01 16:06:15 +0200

    Setup menu

---
+++ Scripts/title_screen.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file('res://Scripts/title_screen.tscn') #change to correct screen later
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_option_button_item_selected(index):
+	pass # Replace with function body.
