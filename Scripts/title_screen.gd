c5652fd0a0791fe0676d5306a1d1d307581e4b1a
TimothyTichelaar <21108366@student.hhs.nl>
2026-07-01 20:28:01 +0200

    Menu domino color fix

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -15,3 +15,17 @@ func _on_quit_button_pressed():
 func _on_option_button_item_selected(index):
 	level = index + 1
 	pass # Replace with function body.
+
+
+func _on_option_button_pressed():
+	var popup = $OptionButton.get_popup()
+
+	# Wait until Godot has positioned the popup.
+	await get_tree().process_frame
+
+	var pos = popup.position
+	pos.x -= popup.size.x  + 59     # Move it to the left of the button.
+	# Optional: align the top edges.
+	pos.y = $OptionButton.global_position.y + 77
+
+	popup.position = pos
