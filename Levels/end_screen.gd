1edc5e9e47f768e2742580898205cbd6c9adb3fd
Mark Wilbrink | Wollepluis <165830976+Wollepluis@users.noreply.github.com>
2026-07-05 15:37:28 +0200

    Level 5 + end screen

--- Levels/end_screen.gd
+++ Levels/end_screen.gd
@@ -1,36 +1,16 @@
-class_name Level
 extends Node
 
 @export var objectsNode: Node
-var dominos: Array[Domino] = []
 
 @export var celebrations: Array[Celebrations] = []
 
-signal level_complete()
 @onready var juichen: AudioStreamPlayer2D = $Juichen
 
 @onready var btn = get_node("Objects/Reset")
 @onready var btn2 = get_node("Objects/Advance")
 
 func _ready() -> void:
-	for child in objectsNode.get_children():
-		if child is Domino:
-			dominos.append(child)
-			child.has_fallen.connect(handle_fallen_domino)
-
-func handle_fallen_domino(domino: Domino) -> void:
-	var index = dominos.find(domino)
-	if index >= 0:
-		dominos.remove_at(index)
-	
-	if dominos.size() == 0:
-		juichen.play()
-		level_complete.emit()
-		celebrate()
-
-func celebrate() -> void:
 	btn2.visible = true
-	btn.visible = false
 	for celebration in celebrations:
 		celebration.start()
 
